a2b4ba80a8d655194802620d5b889cd7cff4202b
rishavemail786 <rishavemail786@gmail.com>
2022-09-05 16:35:54 +0530

    SOLUTION DONE

--- src/main/java/com/jap/validator/CredentialsValidatorImpl.java
+++ src/main/java/com/jap/validator/CredentialsValidatorImpl.java
@@ -1,10 +1,16 @@
 package com.jap.validator;
 
 public class CredentialsValidatorImpl {
-    public String validateUser(String name, String password){
-        return "";
+    public String validateUser(String name, String password) {
+        CredentialsValidator credentialsValidator =(userId, password1) -> {
+            if (userId.equals("admin")&& password1.equals("1234")){
+                return "valid user";
+            }else {
+                return "invalid user";
+            }
+        };
+
+
+        return credentialsValidator.validator(name,password) ;
     }
-
-
-
 }
